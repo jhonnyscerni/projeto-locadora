d2dbd0245eb068a718ee2f1ab989721d72c6fbce
jhonnyscerni <jhonnyscerni@gmail.com>
2017-01-16 08:50:50 -0300

    Exemplo Consulta Criteria - JUNIT

--- projeto-locadora/src/test/java/com/algaworks/projeto/criteria/ExemplosCriteria.java
+++ projeto-locadora/src/test/java/com/algaworks/projeto/criteria/ExemplosCriteria.java
@@ -1,5 +1,56 @@
 package com.algaworks.projeto.criteria;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.algaworks.projeto.model.Carro;
+
 public class ExemplosCriteria {
 
+	private static EntityManagerFactory factory;
+	
+	private EntityManager manager;
+	
+	@BeforeClass
+	public static void init() {
+		factory = Persistence.createEntityManagerFactory("AlgaWorksPU");
+	}
+	
+	@Before
+	public void setUp() {
+		this.manager = factory.createEntityManager();
+	}
+	
+	@After
+	public void tearDown() {
+		this.manager.close();
+	}
+	
+	@Test
+	public void projecoes() {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<String> criteriaQuery = builder.createQuery(String.class);
+		
+		Root<Carro> carro = criteriaQuery.from(Carro.class);
+		criteriaQuery.select(carro.<String>get("placa"));
+		
+		TypedQuery<String> query = manager.createQuery(criteriaQuery);
+		List<String> placas = query.getResultList();
+		
+		for (String placa : placas) {
+			System.out.println(placa);
+		}
+	}
 }
